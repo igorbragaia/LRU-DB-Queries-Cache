787565805d9bbe43681fea119d781b1996eb5167
Igor Bragaia <igor.bragaia@gmail.com>
2019-04-04 00:15:54 -0300

    adding cache to queries

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,2 +1,3 @@
 g++ -std=c++11 $1 -lpqxx -lpq -o your-exec
 ./your-exec
+rm ./your-exec
--- queries.cpp
+++ queries.cpp
@@ -3,19 +3,48 @@
 using namespace std;
 
 
+class LRUcache{
+private:
+  unordered_map<string, vector<vector<string> > >hashtable;
+public:
+  bool hasKey(string query){
+    return hashtable.find(query) != hashtable.end();
+  }
+
+  vector<vector<string> > get(string query){
+    if(hasKey(query))
+      return hashtable[query];
+    else
+      return vector<vector<string> >();
+  }
+
+  void put(string query, vector<vector<string> > &response){
+    if(!hasKey(query)){
+      hashtable[query] = response;
+    }
+  }
+};
+
+
 class Postgres{
 private:
   string connection_str;
+  LRUcache lru_cache;
+  bool cache;
 public:
-  Postgres(){
+  Postgres(bool _cache){
     string host = getenv ("HOST_NODECRUD");
     string dbname = getenv ("DATABASE_NODECRUD");
     string user = getenv ("USER_NODECRUD");
     string password = getenv ("PASSWORD_NODECRUD");
     connection_str = "host='"+ host + "' port='5432' dbname='"+ dbname +"' user='"+ user +"' password='" + password +"'";
+    cache = _cache;
   }
 
   vector<vector<string> > executeQuery(string query){
+    if(cache and lru_cache.hasKey(query))
+      return lru_cache.get(query);
+
     vector<vector<string> > output;
     vector<string> single_output;
     try {
@@ -28,10 +57,12 @@ public:
             single_output.push_back(to_string(ccc));
           output.push_back(single_output);
         }
+        if(cache)
+          lru_cache.put(query, output);
     }
     catch(const exception &e)
     {
-        std::cerr << e.what() << std::endl;
+        cerr << e.what() << std::endl;
     }
     return output;
   }
@@ -45,16 +76,34 @@ public:
   }
 };
 
+
 int main()
 {
     string query = "SELECT id, name, email  FROM users";
-    Postgres postgres;
-    vector<vector<string> > response = postgres.executeQuery(query);
+    Postgres postgres(true);
+    vector<vector<string> > response;
+    clock_t time_a, time_b;
+    unsigned int total_time_ticks;
+
+    time_a = clock();
+    response = postgres.executeQuery(query);
     postgres.printQueryOutput(response);
-    // PRINT EXAMPLE - data can be seen also at https://goo.gl/oeRbiQ
+    time_b = clock();
+    total_time_ticks = (unsigned int)(time_b - time_a);
+    cout << total_time_ticks << " ms" << endl;
+
+    time_a = clock();
+    response = postgres.executeQuery(query);
+    postgres.printQueryOutput(response);
+    time_b = clock();
+    total_time_ticks = (unsigned int)(time_b - time_a);
+    cout << total_time_ticks << " ms" << endl;
+
+    // OUTPUT EXAMPLE - data can be seen also at https://goo.gl/oeRbiQ
     // 17 Luis Alberto Bragaia luis.bragaia@terra.com.br
     // 18 Mexicano mexicano@palantir.com
     // 20 Pianista vitor.arruda@ga.ita.br
     // 22 rfdgsd fsdf@gfmdl
+
     return 0;
 }
