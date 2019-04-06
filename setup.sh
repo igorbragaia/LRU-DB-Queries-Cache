e71c737a7bfbadc1cf99d8baa1ccb42bd69a49a1
igorbragaia <igor.bragaia@gmail.com>
2019-04-06 14:00:45 -0300

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 sudo apt-get update
 sudo apt-get install g++
+wget http://pqxx.org/download/software/libpqxx/libpqxx-4.0.tar.gz
+tar xvfz libpqxx-4.0.tar.gz
+cd libpqxx-4.0
+# ./configure
+# make
+# sudo make install
 sudo apt-get install postgresql postgresql-contrib libpqxx-4.0v5 libpq-dev libpqxx-dev
